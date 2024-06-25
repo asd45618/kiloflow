9f8c1f53babd1ab5773ef711b380d008458ae323
kwang99 <asd45618@naver.com>
2024-06-25 13:05:45 +0900

    음식 등록 페이지 초기 UI

--- pages/food/register/index.tsx
+++ pages/food/register/index.tsx
@@ -1,3 +1,42 @@
+import styles from '../../../styles/foodRegister.module.css';
+
 export default function foodRegister() {
-  return <div>등록</div>;
+  return (
+    <div className={styles.register__wrapper}>
+      <div className={styles.register__title}>
+        <p>음식 이름</p>
+        <input type='text' />
+      </div>
+      <div className={styles.register__img}>
+        <span>사진</span>
+        <input type='file' />
+      </div>
+      <div className={styles.register__info}>
+        <div>
+          <p>단백질</p>
+          <input type='number' />
+          <span> g</span>
+        </div>
+        <div>
+          <p>탄수화물</p>
+          <input type='number' />
+          <span> g</span>
+        </div>
+        <div>
+          <p>지방</p>
+          <input type='number' />
+          <span> g</span>
+        </div>
+        <div>
+          <p>열량</p>
+          <input type='number' />
+          <span> kcal</span>
+        </div>
+      </div>
+      <div className={styles.register__btn}>
+        <button>취소</button>
+        <button>등록</button>
+      </div>
+    </div>
+  );
 }

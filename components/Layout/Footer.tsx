02abb95a600e2054cd1f5b16cf2e58fedf5408b9
asd45618 <asd45618@naver.com>
2024-07-01 00:03:11 +0900

    푸터 디자인

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,10 +1,13 @@
 import {
   faCirclePlus,
+  faDumbbell,
   faHouse,
   faUser,
+  faUtensils,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const FooterWrapper = styled.div`
@@ -12,27 +15,66 @@ const FooterWrapper = styled.div`
   bottom: 2.5%;
   left: 0;
   width: 100%;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
   font-size: 32px;
-  border-top: 1px solid #a1a1a1;
   padding-top: 10px;
+  .footer__top {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 10px;
+    svg {
+      width: 40px;
+      height: 40px;
+      margin: 0 10px;
+      border: 1px solid #0e5b10;
+      border-radius: 50%;
+      padding: 10px;
+      background-color: rgba(14, 91, 16, 0.3);
+    }
+  }
+  .footer__bottom {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    border-top: 1px solid #a1a1a1;
+  }
   a {
     color: #000;
   }
 `;
 
 export default function Footer() {
+  const [hideMenu, setHideMenu] = useState(false);
+
+  useEffect(() => {
+    setHideMenu(false);
+  }, []);
+
   return (
     <FooterWrapper>
-      <Link href='/'>
-        <FontAwesomeIcon icon={faHouse} />
-      </Link>
-      <FontAwesomeIcon icon={faCirclePlus} />
-      <Link href=''>
-        <FontAwesomeIcon icon={faUser} />
-      </Link>
+      {hideMenu ? (
+        <div className='footer__top'>
+          <Link href='/food/list'>
+            <FontAwesomeIcon icon={faUtensils} />
+          </Link>
+          <Link href='/exercise/list'>
+            <FontAwesomeIcon icon={faDumbbell} />
+          </Link>
+        </div>
+      ) : (
+        ""
+      )}
+      <div className='footer__bottom'>
+        <Link href='/'>
+          <FontAwesomeIcon icon={faHouse} />
+        </Link>
+        <div onClick={() => setHideMenu(!hideMenu)}>
+          <FontAwesomeIcon icon={faCirclePlus} />
+        </div>
+        <Link href=''>
+          <FontAwesomeIcon icon={faUser} />
+        </Link>
+      </div>
     </FooterWrapper>
   );
 }

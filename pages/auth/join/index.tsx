bfcd5b51e26ec1c604a4cd19c81d95503998a9b5
Pear-HyunJI <ponyo3764@gmail.com>
2024-07-07 02:53:19 +0900

    join 수정중

--- pages/auth/join/index.tsx
+++ pages/auth/join/index.tsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import Link from "next/link";
 import Modal from "react-modal";
 import Image from "next/image";
+import classnames from "classnames";
 import styles from "../../../styles/components.module.css";
+import styled from "styled-components";
 import minion1 from "../../../public/minion1.png";
 import minion2 from "../../../public/minion2.png";
 import minion3 from "../../../public/minion3.png";
@@ -14,6 +17,66 @@ import minion8 from "../../../public/minion8.png";
 import minion9 from "../../../public/minion9.png";
 import minion10 from "../../../public/minion10.png";
 import minion11 from "../../../public/minion11.png";
+import logo from "../../../public/kiloflow1.png";
+
+const JoinBlock = styled.div`
+  max-width: 345px;
+  margin: 50px auto;
+  text-align: center;
+  color: gray;
+  .top {
+    margin: 25px 0;
+    p {
+      margin: 10px 0;
+    }
+  }
+  table {
+    width: 100%;
+    td {
+      padding: 10px;
+      text-align: center;
+      position: relative;
+      .image {
+        display: inline-block;
+        background-color: #cedecf;
+        border-radius: 50%;
+        width: 150px;
+        height: 150px;
+      }
+    }
+  }
+  .link__join {
+    .text__color {
+      color: #6e9c6f;
+    }
+  }
+  .buttonContainer {
+    position: absolute;
+    top: 10px; /* Adjust the top position as needed */
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background: white;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    padding: 10px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+    z-index: 10;
+  }
+
+  .uploadLabel {
+    cursor: pointer;
+  }
+
+  .imageGrid {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(50px, 1fr));
+    gap: 10px;
+  }
+
+  .imageItem {
+    cursor: pointer;
+  }
+`;
 
 const minionImages = [
   minion1,
@@ -36,7 +99,7 @@ const Join = () => {
   const [nickname, setNickname] = useState("");
   const [profileImage, setProfileImage] = useState<File | string>(minion1.src);
   const [profilePreview, setProfilePreview] = useState<string>(minion1.src);
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [showButtons, setShowButtons] = useState(false);
   const [imageSelectModalIsOpen, setImageSelectModalIsOpen] = useState(false);
   const [emailMessage, setEmailMessage] = useState("");
   const [nicknameMessage, setNicknameMessage] = useState("");
@@ -113,12 +176,6 @@ const Join = () => {
     formData.append("nickname", nickname);
     formData.append("profile_image", profileImage);
 
-    // if (typeof profileImage === "string") {
-    //   formData.append("profile_image", profileImage);
-    // } else {
-    //   formData.append("profile_image", profileImage);
-    // }
-
     try {
       const res = await fetch("/api/auth/join", {
         method: "POST",
@@ -137,14 +194,14 @@ const Join = () => {
   };
 
   const handleProfileImageClick = () => {
-    setModalIsOpen(true);
+    setShowButtons((prevState) => !prevState);
   };
 
   const handleImageSelect = (src: string) => {
     setProfilePreview(src);
     setProfileImage(src);
     setImageSelectModalIsOpen(false);
-    setModalIsOpen(false);
+    setShowButtons(false);
   };
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -153,125 +210,208 @@ const Join = () => {
       setProfileImage(file);
       const fileUrl = URL.createObjectURL(file);
       setProfilePreview(fileUrl);
-      setModalIsOpen(false);
+      setShowButtons(false);
     }
   };
 
+  const handleOutsideClick = (e: MouseEvent) => {
+    if (
+      !(e.target as HTMLElement).closest(".buttonContainer") &&
+      !(e.target as HTMLElement).closest(".profilePreview")
+    ) {
+      setShowButtons(false);
+    }
+  };
+
+  useEffect(() => {
+    if (showButtons) {
+      document.addEventListener("click", handleOutsideClick);
+    } else {
+      document.removeEventListener("click", handleOutsideClick);
+    }
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [showButtons]);
+
   return (
-    <div className={styles.container}>
-      <h1 className="text-3xl font-bold mb-4">회원가입</h1>
-      <form onSubmit={handleSubmit} className={styles.form}>
-        <div className={styles.formGroup}>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          {emailMessage && <p className={styles.message}>{emailMessage}</p>}
+    <JoinBlock className={styles.container}>
+      <form onSubmit={handleSubmit}>
+        <div className="top">
+          <Image src={logo} alt="logo" />
+
+          <p>건강한 흐름속에서 건강한 삶을 시작해보세요.</p>
         </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="confirmPassword">Confirm Password:</label>
-          <input
-            type="password"
-            id="confirmPassword"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
-          {passwordMessage && (
-            <p className={styles.message}>{passwordMessage}</p>
-          )}
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="nickname">Nickname:</label>
-          <input
-            type="text"
-            id="nickname"
-            value={nickname}
-            onChange={(e) => setNickname(e.target.value)}
-            required
-          />
-          {nicknameMessage && (
-            <p className={styles.message}>{nicknameMessage}</p>
-          )}
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="profile_image">Profile Image:</label>
-          <div
-            className={styles.profilePreview}
-            onClick={handleProfileImageClick}
-          >
-            <Image
-              src={profilePreview}
-              alt="Profile Preview"
-              width={100}
-              height={100}
-            />
-          </div>
-          <input
-            type="file"
-            id="profile_image"
-            style={{ display: "none" }}
-            onChange={handleFileChange}
-          />
-        </div>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <div
+                  className={`${styles.profilePreview} profilePreview`}
+                  onClick={handleProfileImageClick}
+                >
+                  <Image
+                    className="image"
+                    src={profilePreview}
+                    alt="Profile Preview"
+                    width={100}
+                    height={100}
+                  />
+                </div>
+                <label htmlFor="profile_image">
+                  프로필 사진을 클릭하여 변경하세요.{" "}
+                </label>
+                <input
+                  type="file"
+                  id="profile_image"
+                  style={{ display: "none" }}
+                  onChange={handleFileChange}
+                />
+                {showButtons && (
+                  <div className="buttonContainer">
+                    <button onClick={() => setImageSelectModalIsOpen(true)}>
+                      기본이미지 선택
+                    </button>
+                    <label
+                      htmlFor="customProfileImage"
+                      className={styles.uploadLabel}
+                    >
+                      나만의 프로필 이미지 선택
+                    </label>
+                    <input
+                      type="file"
+                      id="customProfileImage"
+                      style={{ display: "none" }}
+                      onChange={handleFileChange}
+                    />
+                  </div>
+                )}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <input
+                  className={styles.input__big}
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="이메일 주소"
+                  required
+                />
+
+                {emailMessage && (
+                  <p
+                    className={classnames(styles.message, {
+                      [styles.error]: emailMessage.includes("이미 존재하는"),
+                      [styles.success]: emailMessage.includes("사용가능한"),
+                    })}
+                  >
+                    {emailMessage}
+                  </p>
+                )}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <input
+                  className={styles.input__big}
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="비밀번호"
+                  required
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <input
+                  className={styles.input__big}
+                  type="password"
+                  id="confirmPassword"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  placeholder="비밀번호 확인"
+                  required
+                />
+                {passwordMessage && (
+                  <p
+                    className={classnames(styles.message, {
+                      [styles.error]:
+                        passwordMessage.includes("일치하지 않습니다"),
+                      [styles.success]: passwordMessage.includes("일치합니다"),
+                    })}
+                  >
+                    {passwordMessage}
+                  </p>
+                )}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <input
+                  className={styles.input__big}
+                  type="text"
+                  id="nickname"
+                  value={nickname}
+                  onChange={(e) => setNickname(e.target.value)}
+                  placeholder="닉네임"
+                  required
+                />
+                {nicknameMessage && (
+                  <p
+                    className={classnames(styles.message, {
+                      [styles.error]: nicknameMessage.includes("이미 존재하는"),
+                      [styles.success]: nicknameMessage.includes("사용가능한"),
+                    })}
+                  >
+                    {nicknameMessage}
+                  </p>
+                )}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
         {error && <p className={styles.error}>{error}</p>}
-        <button type="submit" className={styles.btn}>
+        <button type="submit" className={styles.button__big}>
           회원가입
         </button>
       </form>
+      <div className="link__join">
+        <p>
+          이미 계정이 있으신가요?&nbsp;&nbsp;
+          <Link href="/auth/login">
+            <button className="text__color">로그인</button>
+          </Link>
+        </p>
+      </div>
 
-      <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
-        <h2>프로필 이미지 선택</h2>
-        <button onClick={() => setImageSelectModalIsOpen(true)}>
-          기본이미지 선택
-        </button>
-        <label htmlFor="customProfileImage" className={styles.uploadLabel}>
-          나만의 프로필 이미지 선택
-        </label>
-        <input
-          type="file"
-          id="customProfileImage"
-          style={{ display: "none" }}
-          onChange={handleFileChange}
-        />
-        <button onClick={() => setModalIsOpen(false)}>닫기</button>
-      </Modal>
-
-      <Modal
-        isOpen={imageSelectModalIsOpen}
-        onRequestClose={() => setImageSelectModalIsOpen(false)}
-      >
-        <h2>기본 이미지 선택</h2>
-        <div className={styles.imageGrid}>
-          {minionImages.map((img, index) => (
-            <div key={index} className={styles.imageItem}>
-              <Image
-                src={img}
-                alt={`Minion ${index + 1}`}
-                width={50}
-                height={50}
-                onClick={() => handleImageSelect(img.src)}
-              />
-            </div>
-          ))}
-        </div>
-        <button onClick={() => setImageSelectModalIsOpen(false)}>닫기</button>
-      </Modal>
-    </div>
+      {imageSelectModalIsOpen && (
+        <Modal
+          isOpen={imageSelectModalIsOpen}
+          onRequestClose={() => setImageSelectModalIsOpen(false)}
+        >
+          <h2>기본 이미지 선택</h2>
+          <div className={styles.imageGrid}>
+            {minionImages.map((img, index) => (
+              <div key={index} className={styles.imageItem}>
+                <Image
+                  src={img}
+                  alt={`Minion ${index + 1}`}
+                  width={50}
+                  height={50}
+                  onClick={() => handleImageSelect(img.src)}
+                />
+              </div>
+            ))}
+          </div>
+          <button onClick={() => setImageSelectModalIsOpen(false)}>닫기</button>
+        </Modal>
+      )}
+    </JoinBlock>
   );
 };
 
